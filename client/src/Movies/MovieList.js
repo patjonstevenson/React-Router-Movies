7b06ef2d9f677fdcf07338712b6df4d9cef7d641
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-09-17 19:27:15 -0400

    added formatting to index.css

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -32,22 +32,24 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <Link to={`/movies/${movie.id}`} className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
+    <div className="movie-card">
+      <Link className="movie-link" to={`/movies/${movie.id}`}>
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
         </div>
-      ))}
-    </Link>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </Link>
+    </div>
   );
 }
 
